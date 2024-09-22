4400b616ab882ea8f2e5d6b32358cbe26ed33533
Anshuman10703 <152962922+Anshuman10703@users.noreply.github.com>
2024-09-22 21:18:01 +0530

    Create  Caesar Cipher (Encryption and Decryption).java

---
+++ Caesar Cipher (Encryption and Decryption).java
@@ -0,0 +1,26 @@
+public class CaesarCipher {
+
+    public String encrypt(String input, int key) {
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
+        
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(Character.toUpperCase(currChar));
+            
+            if (idx != -1) {
+                char newChar = shiftedAlphabet.charAt(idx);
+                if (Character.isLowerCase(currChar)) {
+                    newChar = Character.toLowerCase(newChar);
+                }
+                encrypted.setCharAt(i, newChar);
+            }
+        }
+        return encrypted.toString();
+    }
+    
+    public String decrypt(String input, int key) {
+        return encrypt(input, 26 - key);
+    }
+}
